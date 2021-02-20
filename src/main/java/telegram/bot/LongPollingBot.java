542847bdc15e344b244140fb9b64fb2db11c86bf
Gircen <gircen666@gmail.com>
2021-02-21 00:02:34 +0300

    initial commit

---
+++ src/main/java/telegram/bot/LongPollingBot.java
@@ -0,0 +1,34 @@
+package telegram.bot;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Component
+public class LongPollingBot extends TelegramLongPollingBot {
+    @Value("${bot.name}")
+    @Getter
+    private String botUsername;
+
+    @Value("${bot.token}")
+    @Getter
+    private String botToken;
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        try {
+            SendMessage sendMessage = new SendMessage();
+            sendMessage.setChatId(String.valueOf(update.getMessage().getChatId()));
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.append("hi ").append(update.getMessage().getChat().getFirstName()).append(" ").append(update.getMessage().getChat().getLastName());
+            sendMessage.setText(stringBuilder.toString());
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/telegram/bot/WebHookBot.java
@@ -0,0 +1,28 @@
+package telegram.bot;
+
+import org.telegram.telegrambots.bots.TelegramWebhookBot;
+import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+public class WebHookBot extends TelegramWebhookBot {
+
+    @Override
+    public String getBotUsername() {
+        return null;
+    }
+
+    @Override
+    public String getBotToken() {
+        return null;
+    }
+
+    @Override
+    public BotApiMethod onWebhookUpdateReceived(Update update) {
+        return null;
+    }
+
+    @Override
+    public String getBotPath() {
+        return null;
+    }
+}
